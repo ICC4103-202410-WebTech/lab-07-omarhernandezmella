70fa527d938f2a28ee42238f1547fff9068b76af
omarhernandezmella <omar@omarhm>
2024-04-22 14:44:27 -0400

    Entrega parcial

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,13 @@
 class Post < ApplicationRecord
-    
+    belong_to :user
+
+    has_many :post_tags
+    has_many :tags, through: :post_tags
+
+    belongs_to :parent_post, class_name: "Post", optional: true
+    has_many :child_posts, class_name: "Post", foreign_key: "parent_post_id"
+
+    validates :name, presence: true
+    validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@gmail\.com\z/i }
+    validates :password, presence: true, length: { minimum: 6 }
 end
---
+++ db/migrate/20240422180317_add_parent_post_reference_to_posts.rb
@@ -0,0 +1,5 @@
+class AddParentPostReferenceToPosts < ActiveRecord::Migration[7.1]
+  def change
+    add_reference :posts, :parent_post, foreign_key: { to_table: :posts }
+  end
+end
